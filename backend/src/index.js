8817e2dda45d572ff12250e0b6a70fd01b784656
ypavanr <ypavanreddy190@gmail.com>
2025-04-13 18:29:44 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import cors from "cors";
+import env from "dotenv";
+
+const app=express()
+env.config()
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors({
+    origin:process.env.CORS_ORIGIN_URL
+}));
+app.listen(3000,()=>{
+    console.log("server listening on port 3000")
+})
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password:process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+  });
+  db.connect().then(()=>{
+    console.log("connected to database")
+  })
+export default db
